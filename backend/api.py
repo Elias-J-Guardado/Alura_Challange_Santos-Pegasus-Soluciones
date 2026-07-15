7a72ec7854768b227d3aec4b34ffc98ac40d75aa
EliasGuardado <eliasjguardado2001@gmail.com>
2026-07-15 15:28:39 -0600

    feat: Add imports in api.py

---
+++ backend/api.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
+from langchain.chains import RetrievalQA
+from langchain_core.prompts import PromptTemplate
